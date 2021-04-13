65f1e23500a2e2f2e2539f0905ac9a106d13c694
okerekechinweotito <you@example.com>
2021-04-13 02:50:24 +0100

    add header

--- js/app.js
+++ js/app.js
@@ -1,13 +1,97 @@
-const formActions =  document.querySelectorAll('.form-action button');
+const formActions = document.querySelectorAll(".form-action button");
 
-formActions.forEach(button => {
-  button.addEventListener('click', e => {
+formActions.forEach((button) => {
+  button.addEventListener("click", (e) => {
     e.preventDefault();
-    M.toast({ html: 'Sign Up & Sign In buttons are disabled!' });
+    M.toast({ html: "Sign Up & Sign In buttons are disabled!" });
 
-    formActions.forEach(button => button.setAttribute('disabled', 'disabled'));
+    formActions.forEach((button) =>
+      button.setAttribute("disabled", "disabled")
+    );
     setTimeout(() => {
-      formActions.forEach(button => button.removeAttribute('disabled'));
-    }, 4e3)
-  })
-})
+      formActions.forEach((button) => button.removeAttribute("disabled"));
+    }, 4e3);
+  });
+});
+
+// Helper Functions
+const slideUp = (target, duration = 500) => {
+  target.style.transitionProperty = "height, margin, padding";
+  target.style.transitionDuration = duration + "ms";
+  target.style.boxSizing = "border-box";
+  target.style.height = target.offsetHeight + "px";
+  target.offsetHeight;
+  target.style.overflow = "hidden";
+  target.style.height = 0;
+  target.style.paddingTop = 0;
+  target.style.paddingBottom = 0;
+  target.style.marginTop = 0;
+  target.style.marginBottom = 0;
+  window.setTimeout(() => {
+    target.style.display = "none";
+    target.style.removeProperty("height");
+    target.style.removeProperty("padding-top");
+    target.style.removeProperty("padding-bottom");
+    target.style.removeProperty("margin-top");
+    target.style.removeProperty("margin-bottom");
+    target.style.removeProperty("overflow");
+    target.style.removeProperty("transition-duration");
+    target.style.removeProperty("transition-property");
+    //alert("!");
+  }, duration);
+};
+
+const slideDown = (target, duration = 500) => {
+  target.style.removeProperty("display");
+  let display = window.getComputedStyle(target).display;
+
+  if (display === "none") display = "block";
+
+  target.style.display = display;
+  let height = target.offsetHeight;
+  target.style.overflow = "hidden";
+  target.style.height = 0;
+  target.style.paddingTop = 0;
+  target.style.paddingBottom = 0;
+  target.style.marginTop = 0;
+  target.style.marginBottom = 0;
+  target.offsetHeight;
+  target.style.boxSizing = "border-box";
+  target.style.transitionProperty = "height, margin, padding";
+  target.style.transitionDuration = duration + "ms";
+  target.style.height = height + "px";
+  target.style.removeProperty("padding-top");
+  target.style.removeProperty("padding-bottom");
+  target.style.removeProperty("margin-top");
+  target.style.removeProperty("margin-bottom");
+  window.setTimeout(() => {
+    target.style.removeProperty("height");
+    target.style.removeProperty("overflow");
+    target.style.removeProperty("transition-duration");
+    target.style.removeProperty("transition-property");
+  }, duration);
+};
+const slideToggle = (target, duration = 500) => {
+  if (window.getComputedStyle(target).display === "none") {
+    return slideDown(target, duration);
+  } else {
+    return slideUp(target, duration);
+  }
+};
+
+// Mobile Navigation
+const nav = document.querySelector(".nav");
+const header = document.querySelector("header");
+
+document.querySelector(".trigger").addEventListener("click", () => {
+  header.classList.toggle("open");
+  // nav.slideToggle(300);
+  slideToggle(nav, 300);
+});
+
+// Sticky Navigation
+window.addEventListener("scroll", () => {
+  window.pageYOffset < 50
+    ? header.classList.remove("sticky")
+    : header.classList.add("sticky");
+});
